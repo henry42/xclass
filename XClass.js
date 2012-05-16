f8884f7dd327c0fe84ec7cf0317b768e088995c0
henry <henrypares@163.com>
2012-05-17 00:03:33 +0800

    overridden() can not call super class's method

--- XClass.js
+++ XClass.js
@@ -1,80 +1,104 @@
 (function( w ){
 
 	var OBJECT_EACH = function( obj , func , scope ){
-		for( var x in obj )
+		for( var x in obj  )
 			if( obj.hasOwnProperty(x) )
 				func.call( scope || w , x , obj[x] );
 	};
 
 	var ARRAY_EACH = function( obj , func , scope ){
-		for( var i = 0 , len = obj.length ; i < len ; i++ )
-			func.call( scope || w , i , obj[i] );
+		for( var i = 0 , len = obj && obj.length || 0 ; i < len ; i++ )
+			func.call( scope || w , obj[i] , i );
 	};
 
-	var EXTEND_CALLER = function( params ){
+	var EXTEND = function( params , canOverride , keepHandler ){
 		OBJECT_EACH( params , function( name , value  ){
-			var prev = this.fn[ name ];
-			this.fn[ name ] = value;
+			var prev = this.prototype[ name ];
+            if( prev && !canOverride )
+                return;
+			this.prototype[ name ] = value;
 			value.$name = name;
 			value.$owner = this;
-			if( prev )
+			if( prev && keepHandler )
 				value.$prev = prev;
 		} , this );
 	};
 
-	var OVERRIDE_CALLER = function( params ){
-		EXTEND_CALLER.call( this , params  );
-	};
 
-	var MIXIN_CALLER = function( obj ){
-		this.mixins.push( obj );
-		EXTEND_CALLER.call( this , obj.fn  );
-	};
 
-	var OVERRIDDEN_CALLER = function(){
-		var caller = OVERRIDDEN_CALLER.caller;
-		if( !caller.$prev )
-			throw('no overridden method');
-		else
-			return caller.$prev.apply( this , arguments );
-	};
+   var XNative = function( params ){
 
-	var PARENT_CALLER = function(){
-		var caller = PARENT_CALLER.caller , superClass = caller.$owner && caller.$owner.superclass , name = caller.$name;
-		if( !superClass )
-			throw('no super class');
-		else{
-			if( !name )
-				throw('unknown method name');
-			else if( !superClass.fn[ name ] )
-				throw('super class has no ' + name + ' method');
-			else
-				return superClass.fn[ name ].apply( this , arguments );
-		}
-	};
+   };
+
+    XNative.mixin = function( object ){
+        this.mixins.push( object );
+        this.implement( object.prototype );
+    };
+
+    XNative.implement = function( params ){
+        EXTEND.call( this , params , true , true );
+    };
+
+    XNative.prototype = {
+        overridden : function(){
+            var caller = this.overridden.caller;
+            if( !caller.$prev )
+                throw('no overridden method');
+            else
+                return caller.$prev.apply( this , arguments );
+        },
+        parent : function(){
+            var caller = this.parent.caller , superClass = caller.$owner && caller.$owner.superclass , name = caller.$name;
+            if( !superClass )
+                throw('no super class');
+            else{
+                if( !name )
+                    throw('unknown method name');
+                else if( !superClass.prototype[ name ] )
+                    throw('super class has no ' + name + ' method');
+                else
+                    return superClass.prototype[ name ].apply( this , arguments );
+            }
+        }
+    };
+
+
 	
 	var PROCESSOR = {
-		'statics' : function( newClass , value  ){
-			OBJECT_EACH( value , function( k , v ){
+		'statics' : function( newClass , methods  ){
+			OBJECT_EACH( methods , function( k , v ){
 				newClass[ k ] = v;
 			});
 		},
-		'extend' : function( newClass , value ){
-			var superClass = value , fn = newClass.fn , superFn = superClass.fn;
-			newClass.superclass = fn.superclass = value;
-			newClass.mixins.push.apply( newClass.mixins , superClass.mixins );
-			OBJECT_EACH( superFn , function( k ,v ){
-				fn[ k ] = v;
+		'extend' : function( newClass , superClass ){
+			var superClass = superClass || XNative , prototype = newClass.prototype , superPrototype = superClass.prototype;
+
+            //process mixins
+            newClass.mixins = [];
+            if( superClass.mixins )
+			    newClass.mixins.push.apply( newClass.mixins , superClass.mixins );
+
+            //process statics
+            OBJECT_EACH( superClass , function( k ,v ){
+                newClass[ k ] = v;
+            })
+
+            //process prototype
+			OBJECT_EACH( superPrototype , function( k ,v ){
+                prototype[ k ] = v;
 			});
+
+            newClass.superclass = prototype.superclass = superClass;
 		},
 		'mixins' : function( newClass , value ){
-			ARRAY_EACH( value , function( k , v ){
+
+			ARRAY_EACH( value , function( v ){
 				newClass.mixin( v );
 			});
 		}
 	};
 
-	var PROCESSOR_KEYS = ['statics','mixins','extend'];
+	var PROCESSOR_KEYS = ['statics','extend','mixins'];
 
     /**
      * @class XClass
@@ -105,33 +129,25 @@
                 else
                     NewClass.singleton = me;
 
-			ARRAY_EACH( NewClass.mixins , function( index , mixin ){
+			ARRAY_EACH( NewClass.mixins , function(  mixin ){
 				mixin.prototype.initialize && mixin.prototype.initialize.apply( me , args );
 			});
 			return me.initialize && me.initialize.apply( me , arguments ) || me;
 		};
 
-		var fn = NewClass.fn = NewClass.prototype;
 
-		fn.parent = PARENT_CALLER;
-		fn.overridden = OVERRIDDEN_CALLER;
-
-		NewClass.override = OVERRIDE_CALLER;
-		NewClass.mixin = MIXIN_CALLER;
-		NewClass.mixins = [];
 		var methods = {};
 
-		ARRAY_EACH( PROCESSOR_KEYS , function( index, key ){
-			if( params[ key ] )
-				PROCESSOR[ key ]( NewClass  , params[ key ] , key );
+		ARRAY_EACH( PROCESSOR_KEYS , function( key ){
+			PROCESSOR[ key ]( NewClass  , params[ key ] , key );
 		});
 
 		OBJECT_EACH( params , function( k , v ){
 			if( !PROCESSOR[ k ] )
 				methods[ k ] = v;
 		});
-		
-		NewClass.override( methods );
+
+        EXTEND.call( NewClass , methods  , true , false );
 
 		return NewClass;
 	}
