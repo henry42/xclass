42e0bf432d0d7f634f4c43a21687661d56eba75f
Henry <henrypares@163.com>
2013-07-04 19:25:32 +0800

    use yuidoc instead of jsdoc

---
+++ doc/api.js
@@ -0,0 +1,18 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "XClass",
+        "XClass.utils",
+        "XNative"
+    ],
+    "modules": [
+        "xclass"
+    ],
+    "allModules": [
+        {
+            "displayName": "xclass",
+            "name": "xclass"
+        }
+    ]
+} };
+});
